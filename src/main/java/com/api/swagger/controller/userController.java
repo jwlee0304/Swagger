c1fe215494c734f7c11844e84af5b43dbffd6244
CFM\jwlee <jwlee@mediawill.com>
2021-12-28 11:50:32 +0900

    FEAT : API 추가~

--- src/main/java/com/api/swagger/controller/userController.java
+++ src/main/java/com/api/swagger/controller/userController.java
@@ -18,6 +18,11 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 @RequestMapping(value="/api")
 public class userController {
+	
+	
+	private static String resultS = "SUCCESS";	
+	private static String resultF = "FAILED";
+	
 
 	@ApiOperation(value="사용자 정보", notes = "사용자 정보 리스트 조회")
 	@GetMapping(value="/getUserList")
@@ -47,6 +52,61 @@ public class userController {
 		obj.put("total", userList.size());
 		obj.put("users", jarr);
 	
+		return obj;
+	}
+	
+	
+	@ApiOperation(value="사용자 직책 정보", notes = "사용자 직책 정보 조회")
+	@GetMapping(value="/getPosition")
+	public JSONObject getPosition(@RequestParam String name) {		
+		
+		String inputName = name;
+		JSONObject obj = new JSONObject();
+		String rMsg = null;
+		String result = null;
+		String position = null;	
+		
+	    List userList = new ArrayList();
+		
+		Map userInfo = new HashMap();
+		userInfo.put("name", "이정원");
+		userInfo.put("age", "33");
+		userInfo.put("gender", "남자");
+		userInfo.put("position", "대리");
+		userList.add(userInfo);
+		
+		Map userInfo2 = new HashMap();
+		userInfo2.put("name", "임규혁");
+		userInfo2.put("age", "28");
+		userInfo2.put("gender", "남자");
+		userInfo2.put("position", "사원");
+		userList.add(userInfo2);
+		
+		if(inputName.isEmpty() || inputName == "" || inputName == null) {
+			rMsg = "사용자 명을 입력해주시기 바랍니다.";
+			result = resultF;
+			System.out.println("사용자 명을 입력해주시기 바랍니다.");
+			obj.put("msg", rMsg);
+		}else {			
+			for(int i=0;i<userList.size();i++) {
+				Map userMap = (HashMap)userList.get(i);
+				String nm = String.valueOf(userMap.get("name"));
+				if(inputName.equals(nm)) {					
+					position = String.valueOf(userMap.get("position"));
+					obj.put("position", position);
+					result = resultS;		
+				}			
+			}
+			
+			if(position == null && position.isEmpty()) {
+				rMsg = "존재하지 않은 사용자입니다.";
+				result = resultF;
+				obj.put("msg", rMsg);
+			}			
+		
+		}		
+		
+		obj.put("result", result);		
 		return obj;
 	}
 }
